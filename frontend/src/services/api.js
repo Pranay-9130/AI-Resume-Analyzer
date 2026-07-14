131a976c588e3ef01c580e84c34ac3b1221ef0fc
Pranay <pranay@gmail.com>
2026-07-14 10:43:19 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-resume-analyzer-gmhw.onrender.com",
+  baseURL: "https://ai-resume-analyzer-ci2b.onrender.com",
 });
 
 export default API;
